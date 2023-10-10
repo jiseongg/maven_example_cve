5af2e7cfd812e47e9fba2e0f50afb67d2a2320c0
Jiseong Bak <jiseongg301@labradorlabs.ai>
2023-10-10 17:27:29 +0900

    add httpcomponents-client

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,6 +1,8 @@
 package com.mycompany.app;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.http.client.utils.URIBuilder;
+
 
 /**
  * Hello world!
@@ -8,10 +10,17 @@ import org.apache.commons.io.FilenameUtils;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) 
     {
-        System.out.println( "Hello World!" );
-        String fileName = FilenameUtils.normalize("//../foo");
-        System.out.println(fileName);
+        try {
+            System.out.println( "Hello World!" );
+            String fileName = FilenameUtils.normalize("//../foo");
+            URIBuilder builder = new URIBuilder();
+            System.out.println(builder.build().toString());
+            System.out.println(fileName);
+        } catch (Exception e) {
+            System.err.println("error occurred");
+            System.err.println(e.toString());
+        }
     }
 }
